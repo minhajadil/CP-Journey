e2dc9d8844f4f06bb51d730afefaafa8e7894ad4
Minhajul Abedin Adil <64237828+minhajadil@users.noreply.github.com>
2024-03-09 17:35:31 +0600

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc148_a/37470450.cpp
@@ -0,0 +1,18 @@
+// Problem: A - Round One
+// Contest: AtCoder - AtCoder Beginner Contest 148
+
+// May the provider keep us content
+
+#include<bits/stdc++.h>
+using namespace std ;
+
+
+int main()
+{
+	int a , b ;
+	cin >> a >>b ;
+	
+	cout << 6-(a+b) << endl ;
+
+
+}
