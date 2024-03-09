9774aa17899769a2106c649f56e250d54dfe4942
Minhajul Abedin Adil <64237828+minhajadil@users.noreply.github.com>
2024-03-09 17:34:32 +0600

    Committing files

---
+++ Atcoder/C++ 20 (gcc 12.2)/abc214_b/48234103.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+
+
+
+
+int main()
+{
+
+  int s,t ; cin >> s >> t ;
+
+
+
+  int cnt=0 ;
+
+
+  for(int i =0 ;i<=s ;i++)
+  {
+    for(int j =0 ;j<=s ;j++)
+    {
+      for(int k=0 ;k<=s ;k++)
+      {
+        if(i*j*k<=t && i+k+j<=s) cnt++ ;
+      }
+    }
+  }
+
+  cout << cnt << endl ;
+
+}
