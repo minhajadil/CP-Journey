b30d9eba9feea531a135d85073dc5d85808d4e6e
Minhajul Abedin Adil <64237828+minhajadil@users.noreply.github.com>
2024-03-09 17:34:57 +0600

    Committing files

---
+++ Atcoder/C++ 20 (gcc 12.2)/dp_b/46003433.cpp
@@ -0,0 +1,114 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+//............// define///.................... //
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define endl "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define vc vector
+#define vi vc<int>
+#define vll vc<ll>
+#define pii pair<int,int>
+#define fr(i,a,b) for(ll i=a;i<=b;i++)
+#define coutv(v) for(auto it:v)cout<<it<<' ';cout<<endl;
+#define cinv(v) for(auto &it:v)cin>>it;
+ll llmin(ll a , ll b){if(a<b) return a ; else return b ;}
+ll llmax(ll a , ll b) {if(a>b) return a ; else return b ;}
+
+
+///...........................//////////////
+
+const int N =1e5+10 ;
+const int x = 1e9+10 ;
+int n ;
+
+int height[N] ;
+int dp[N] ;
+
+
+
+
+void solve()
+{
+
+
+    for(int i =1 ;i<N ;i++)
+    {
+        dp[i] =x ;
+    }
+     cin >> n ; int k ; cin >> k ;
+
+    for(int i =1 ;i<=n ;i++)
+    {
+        cin >> height[i] ;
+    }
+    dp[1] = 0 ;
+
+    for(int i =1 ;i<=n ;i++)
+    {
+        for(int j = i-1, r = k ; j>0 && r>0 ;j-- ,r-- )
+        {
+            dp[i]= min(dp[i],dp[j]+abs(height[i]-height[j])) ;
+        }
+    }
+
+   
+
+    cout << dp[n] << endl ;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main()
+{
+fastio() ;
+  #ifndef ONLINE_JUDGE
+  freopen("Error.in", "w", stderr);
+#endif
+
+
+   int t =1 ;
+   while(t--)
+   {
+
+     solve() ;
+    }
+
+ 
+
+
+
+}
+
+
+
+
+
+
