25e118c4f71482c77b1c6980936059e110dcd9f6
Minhajul Abedin Adil <64237828+minhajadil@users.noreply.github.com>
2024-03-09 17:34:36 +0600

    Committing files

---
+++ Atcoder/C++ 20 (gcc 12.2)/abc193_a/48233746.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+
+
+
+
+void solve()
+{
+  double a, b ; cin >> a >> b ;
+
+
+  cout << setprecision(14) << ((a-b)/a)*100 ;
+  cout << endl ;
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main()
+{
+  solve() ;
+
+
+
+
+}
+
+
+
+
+
+
