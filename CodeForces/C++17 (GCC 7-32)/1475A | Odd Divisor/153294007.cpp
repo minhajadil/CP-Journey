d2c8c04daf2c99c6a227e7ddd0a2dc216b297e91
Minhajul Abedin Adil <64237828+minhajadil@users.noreply.github.com>
2024-03-10 07:02:39 +0600

    Committing files

---
+++ CodeForces/C++17 (GCC 7-32)/1475A | Odd Divisor/153294007.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std ;
+ 
+ 
+int main()
+{
+ 
+   int t ;
+   cin >> t ;
+ 
+   while(t--)
+   {
+      long long  n ;
+      cin >>n  ;
+      
+      if(ceil(log2(n))==floor(log2(n))) 
+      {
+        cout << "NO" << endl ;
+      }
+      else cout << "YES" << endl ;
+ 
+ 
+ 
+   }
+ 
+ 
+ 
+ 
+}
