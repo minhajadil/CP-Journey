284ae33d06840d97fced2be5f07fb4730b13316f
Minhajul Abedin Adil <64237828+minhajadil@users.noreply.github.com>
2024-03-10 07:00:21 +0600

    Committing files

---
+++ CodeForces/C++17 (GCC 7-32)/1722A | Spell Check/170302933.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std ;
+ 
+ 
+int main()
+{
+ int t ;
+ cin >> t ;
+ while(t--)
+ {
+  int n ;
+  cin >> n ;
+  string a ;
+  cin >> a ;
+  sort(a.begin(),a.end()) ;
+  if(a=="Timru") cout << "YES" << endl ;
+  else cout << "NO" << endl ; 
+ 
+ }
+}
