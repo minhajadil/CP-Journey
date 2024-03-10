d1f122a91601ba4f17054cd74a5b26f917d6d1ba
Minhajul Abedin Adil <64237828+minhajadil@users.noreply.github.com>
2024-03-10 07:06:29 +0600

    Committing files

---
+++ CodeForces/C++17 (GCC 7-32)/71A | Way Too Long Words/86633357.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std ;
+ 
+int main() {
+  int t ;
+  cin>> t ;
+ 
+  while(t--) {
+    string a ;
+    cin>> a ;
+ 
+    int len = a.size() ;
+    int leng2= len-1 ;
+    int leng1=len-2 ;
+    if(len>10) cout << a[0] << leng1 <<a[leng2] << endl ;
+    else cout << a << endl ;
+ 
+ 
+ 
+ 
+ 
+  }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+return 0 ;
+}
+ 
+ 
+ 
+ 
+ 
+ 
