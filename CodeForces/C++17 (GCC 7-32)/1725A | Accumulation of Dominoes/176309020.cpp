73540648de21983684f7fd946226b85d13056d3f
Minhajul Abedin Adil <64237828+minhajadil@users.noreply.github.com>
2024-03-10 06:58:33 +0600

    Committing files

---
+++ CodeForces/C++17 (GCC 7-32)/1725A | Accumulation of Dominoes/176309020.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std ;
+ 
+ 
+int main()
+{
+   long long n , m ;
+   cin >> n >> m ;
+ 
+ 
+   
+ 
+if(m>1)   cout << (n)*(m-1) << endl ;
+else cout << n-1 << endl ;
+}
