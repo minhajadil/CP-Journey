987f213f8bc3810ec8da6978b72ccee26756a02c
Minhajul Abedin Adil <64237828+minhajadil@users.noreply.github.com>
2024-03-10 07:02:15 +0600

    Committing files

---
+++ CodeForces/C++17 (GCC 7-32)/443A | Anton and Letters/157732369.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std ;
+ 
+ 
+ 
+int main()
+ 
+ 
+{
+ 
+ 
+    string a; 
+    getline(cin , a) ;
+    int sz = a.size() ;
+    map<char , int > l ;
+    char r ;
+ 
+    for(int i =0 ;i<sz ;i++)
+    {   
+        if(a[i]!='{' && a[i]!='}' &&a[i]!=',' && a[i]!=' '  ) { r=a[i] ;
+           l[r]++ ; }
+ 
+    }
+ 
+    int cnt=0 ;
+ 
+    for(auto it : l)
+    {
+        cnt++ ;
+    }
+ 
+    cout << cnt << endl ;
+ 
+ 
+ 
+ 
+ 
+}
