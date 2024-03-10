4924c2f450056ded55b863e45b32492d96e9cc8b
Minhajul Abedin Adil <64237828+minhajadil@users.noreply.github.com>
2024-03-10 06:41:57 +0600

    Committing files

---
+++ CodeForces/C++20 (GCC 11-64)/1899E | Queue Sort/233248420.cpp
@@ -0,0 +1,116 @@
+#include<bits/stdc++.h>
+using namespace std ;
+ 
+//............// define///.................... //
+ 
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+ 
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define endl "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define vc vector
+#define vi vc<int>
+#define vll vc<ll>
+#define pii pair<int,int>
+#define fr(i,a,b) for(ll i=a;i<=b;i++)
+#define coutv(v) for(auto it:v)cout<<it<<' ';cout<<endl;
+#define cinv(v) for(auto &it:v)cin>>it;
+ll llmin(ll a , ll b){if(a<b) return a ; else return b ;}
+ll llmax(ll a , ll b) {if(a>b) return a ; else return b ;}
+ 
+ 
+///...........................//////////////
+ 
+ 
+ 
+ 
+void solve()
+{
+  int n ; cin >> n ;
+  int a[n] ;
+ 
+ 
+ 
+  for(int i =0 ;i<n ;i++) cin >> a[i] ;
+   int mn = MOD ;
+ 
+  int ind = -1 ;
+ 
+  for(int i =0 ;i<n ;i++)
+  {
+   if(mn>a[i])
+   {
+    ind = i ;
+    mn = a[i] ;
+ 
+   }
+  }
+ 
+  for(int i = ind+2 ;i<n ;i++)
+  {
+   if(a[i-1]>a[i]) 
+   {
+    cout << -1 << endl ;
+    return ;
+   }
+ 
+  }
+ // if(is_sorted(a,a+n)) cout << 0 << endl ;
+   cout << ind << endl ;
+ 
+ 
+  
+}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+int main()
+{
+fastio() ;
+  #ifndef ONLINE_JUDGE
+  freopen("Error.in", "w", stderr);
+#endif
+ 
+ 
+   int t ; cin >> t ;
+   while(t--)
+   {
+ 
+     solve() ;
+    }
+ 
+ 
+ 
+ 
+ 
+}
+ 
+ 
+ 
+ 
+ 
+ 
