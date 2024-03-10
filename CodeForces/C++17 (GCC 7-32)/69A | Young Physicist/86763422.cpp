3540aff0613e7ecb99bad1a1a5fd8bdaa5708368
Minhajul Abedin Adil <64237828+minhajadil@users.noreply.github.com>
2024-03-10 07:06:09 +0600

    Committing files

---
+++ CodeForces/C++17 (GCC 7-32)/69A | Young Physicist/86763422.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std ;
+ 
+int main() {
+int t ;
+cin>> t ;
+int sumx =0 , sumy=0 ,sumz = 0 ;
+while(t--) {
+    int a , b ,c ;
+    cin >> a>> b>> c ;
+ sumx+=a ;
+ sumy+=b ;
+ sumz+=c ;
+ 
+}
+if(sumx==0 && sumy==0 && sumz==0) cout << "YES" << endl ;
+else cout << "NO" << endl ;
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+return 0 ;
+}
