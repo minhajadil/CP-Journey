f6ffdc88458dce4f3566715d6a2da48fe0a5e6cb
Minhajul Abedin Adil <64237828+minhajadil@users.noreply.github.com>
2024-03-10 06:56:30 +0600

    Committing files

---
+++ CodeForces/C++17 (GCC 7-32)/199A | Hexadecimal's theorem/178887334.cpp
@@ -0,0 +1,17 @@
+// Problem: A. Hexadecimal's theorem
+// Contest: Codeforces - Codeforces Round #125 (Div. 2)
+ 
+// May the provider keep us content
+ 
+#include<bits/stdc++.h>
+using namespace std ;
+ 
+ 
+int main()
+{
+ int n ;
+ cin >> n ;
+ cout << 0 << " " << 0 << " " << n << endl ;
+ 
+ 
+}
