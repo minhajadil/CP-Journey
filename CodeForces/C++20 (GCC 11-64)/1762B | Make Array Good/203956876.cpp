99b5084dfbc00a7a70bf732ba0fa3be412370e4c
Minhajul Abedin Adil <64237828+minhajadil@users.noreply.github.com>
2024-03-10 06:48:21 +0600

    Committing files

---
+++ CodeForces/C++20 (GCC 11-64)/1762B | Make Array Good/203956876.cpp
@@ -0,0 +1,122 @@
+#include<bits/stdc++.h>
+using namespace std ;
+ 
+//............// define///.................... //
+ 
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define endl "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define ff first
+#define second ss 
+ 
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+ 
+///...........................//////////////
+////.......... debugger .........................//
+ 
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+ 
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+//////............................./////////////////
+ 
+ 
+ 
+ 
+ 
+ 
+ll nearest_power(ll n)
+{
+    ll x =1 ;
+ 
+    while(x<n)
+    {
+        x*=2 ;
+    }
+    return x ;
+}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+int main()
+{
+fastio() ;
+  #ifndef ONLINE_JUDGE
+  freopen("Error.in", "w", stderr);
+#endif
+ 
+ 
+// prime numbers power seq - 1 2 4 8 16 32
+// if we take any numbers from the seq the min number will always divide the max number
+// why ? because they are both power of 2 and the min has less exponent thant the max
+// 2^3 = 8 , 2^5 = 32 32/8 = 2^2
+ 
+ 
+   int t ; cin >> t ;
+   while(t--)
+   {
+ 
+    int n ;
+    cin >> n ;
+ 
+    int a[n] ; 
+ 
+    for(int i =0 ;i<n ;i++)
+    {
+        cin >> a[i] ;
+    }
+ 
+    cout << n << endl ;
+ 
+    for(int i =0 ;i<n ;i++)
+    {
+        cout << i+1 << " " << nearest_power(a[i])-a[i] << endl ;
+    }
+ 
+ 
+    }
+ 
+}
+ 
+ 
+ 
+ 
+ 
+ 
