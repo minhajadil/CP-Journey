7716a28d7bac9b6e614aca1c712bed6ad70e48a3
Minhajul Abedin Adil <64237828+minhajadil@users.noreply.github.com>
2024-03-10 07:03:26 +0600

    Committing files

---
+++ CodeForces/C++17 (GCC 7-32)/1328A | Divisibility Problem/149883082.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std ;
+ 
+int main()
+{   
+ int t ;
+ cin >>t ;
+ 
+ while(t--)
+ {
+  int a ,b ;
+  cin >> a >> b ;
+ 
+  int rem= a%b ;
+  if(rem!=0)
+  {int add = b-rem ;
+  cout << add << endl ; }
+  else cout << 0 << endl ;
+ 
+ 
+ 
+ 
+ 
+ 
+ }
+ 
+ 
+}
