7c0ec78cd0d09259b7a4adac3ae72d0b0b644957
Minhajul Abedin Adil <64237828+minhajadil@users.noreply.github.com>
2024-03-10 06:44:10 +0600

    Committing files

---
+++ CodeForces/C++20 (GCC 11-64)/1873D | 1D Eraser/224419735.cpp
@@ -0,0 +1,105 @@
+#include<bits/stdc++.h>
+using namespace std ;
+ 
+//............// define///.................... //
+ 
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+ 
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define endl "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define vc vector
+#define vi vc<int>
+#define vll vc<ll>
+#define pii pair<int,int>
+#define fr(i,a,b) for(ll i=a;i<=b;i++)
+#define coutv(v) for(auto it:v)cout<<it<<' ';cout<<endl;
+#define cinv(v) for(auto &it:v)cin>>it;
+ll llmin(ll a , ll b){if(a<b) return a ; else return b ;}
+ll llmax(ll a , ll b) {if(a>b) return a ; else return b ;}
+ 
+ 
+///...........................//////////////
+ 
+ 
+ 
+ 
+void solve()
+{
+  int n , k ; cin >> n >> k ;
+ 
+ 
+  string a ; cin >> a ;
+ 
+  int ans=0 ;
+ 
+ 
+  for(int i =0 ;i<n ;i++)
+  {
+ 
+ 
+    if(a[i]=='B')
+    {
+        ans++ ;
+        i+=k-1 ;
+      //  cout << i << endl ;
+ 
+    }
+  }
+ 
+  cout << ans << endl ;
+  
+}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+int main()
+{
+fastio() ;
+  #ifndef ONLINE_JUDGE
+  freopen("Error.in", "w", stderr);
+#endif
+ 
+ 
+   int t ; cin >> t ;
+   while(t--)
+   {
+ 
+     solve() ;
+    }
+ 
+ 
+ 
+ 
+ 
+}
+ 
+ 
+ 
+ 
+ 
+ 
