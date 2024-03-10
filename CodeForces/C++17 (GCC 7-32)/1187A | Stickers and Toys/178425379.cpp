7c1519f4a0cebdf53b4d9205c5b0e51b6253d646
Minhajul Abedin Adil <64237828+minhajadil@users.noreply.github.com>
2024-03-10 06:56:53 +0600

    Committing files

---
+++ CodeForces/C++17 (GCC 7-32)/1187A | Stickers and Toys/178425379.cpp
@@ -0,0 +1,24 @@
+// Problem: A. Stickers and Toys
+// Contest: Codeforces - Educational Codeforces Round 67 (Rated for Div. 2)
+ 
+// May the provider keep us content
+ 
+#include<bits/stdc++.h>
+using namespace std ;
+ 
+ 
+int main()
+{
+ int t ;
+ cin >> t ;
+ 
+ while(t--)
+ {
+  int n , t , s ;
+  cin >> n >> t >>  s ;
+  cout << max(n-t , n-s)+1 << endl ;
+  
+ }
+ 
+ 
+}
