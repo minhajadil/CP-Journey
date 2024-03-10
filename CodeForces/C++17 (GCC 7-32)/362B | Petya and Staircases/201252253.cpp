ee77bbc431c7ba2c5373b25ce84efc006188cff2
Minhajul Abedin Adil <64237828+minhajadil@users.noreply.github.com>
2024-03-10 06:49:20 +0600

    Committing files

---
+++ CodeForces/C++17 (GCC 7-32)/362B | Petya and Staircases/201252253.cpp
@@ -0,0 +1,134 @@
+#include<bits/stdc++.h>
+using namespace std ;
+ 
+//............// define///.................... //
+ 
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define nline "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+ 
+#define ff first
+#define second ss 
+ 
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+ 
+///...........................//////////////
+////.......... debugger .........................//
+ 
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+ 
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+//////............................./////////////////
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+int main()
+{
+fastio() ;
+  #ifndef ONLINE_JUDGE
+  freopen("Error.in", "w", stderr);
+#endif
+ 
+ 
+ int n , m ;
+ cin >> n >> m ;
+vector<int> a(m) ;
+ 
+ for(int i =0 ;i<m ;i++)
+ {
+    cin >> a[i] ;
+ }
+ 
+ sort(all(a)) ;
+ 
+ if(m==0) 
+ {
+    cout << "YES" << endl ;
+    return 0 ;
+ }
+ 
+ if(a[0]==1 || a[m-1]==n) 
+ {
+    cout << "NO" << endl ;
+    return 0 ;
+ }
+ 
+ 
+debug(a) 
+ int h =0 ;
+ 
+ for(int i=0 ;i<m-2 ;i++)
+ {
+    if(a[i]+1==a[i+1] && a[i+1]+1==a[i+2] )
+    {
+        h=1 ;
+        break ;
+    }
+ }
+ 
+ if(h==1) cout << "NO" << endl ;
+ else cout << "YES" << endl ;
+ 
+ 
+ 
+ 
+ 
+}
+ 
+ 
+ 
+ 
+ 
+ 
