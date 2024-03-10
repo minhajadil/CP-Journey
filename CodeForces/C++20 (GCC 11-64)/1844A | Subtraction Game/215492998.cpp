d53f19702a6cf48b9116993e0637bb4c43fbfd69
Minhajul Abedin Adil <64237828+minhajadil@users.noreply.github.com>
2024-03-10 06:45:17 +0600

    Committing files

---
+++ CodeForces/C++20 (GCC 11-64)/1844A | Subtraction Game/215492998.cpp
@@ -0,0 +1,78 @@
+#include<bits/stdc++.h>
+using namespace std ;
+ 
+//............// define///.................... //
+ 
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define endl "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+ 
+///...........................//////////////
+ 
+ 
+ 
+ 
+void solve()
+{
+  int a , b ; cin >> a >> b ;
+ 
+  if(min(a,b)!=1)
+  cout << min(a,b)-1 << endl ;
+else cout << max(a,b)+1 << endl ;
+ 
+  
+}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+int main()
+{
+fastio() ;
+  #ifndef ONLINE_JUDGE
+  freopen("Error.in", "w", stderr);
+#endif
+ 
+ 
+   int t ; cin >> t ;
+   while(t--)
+   {
+ 
+     solve() ;
+    }
+ 
+ 
+ 
+ 
+ 
+}
+ 
+ 
+ 
+ 
+ 
+ 
