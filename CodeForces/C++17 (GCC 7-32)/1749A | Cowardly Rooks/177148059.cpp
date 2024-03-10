efeb579ab7f4b2729e158fadc3fa8799adb38a8f
Minhajul Abedin Adil <64237828+minhajadil@users.noreply.github.com>
2024-03-10 06:57:53 +0600

    Committing files

---
+++ CodeForces/C++17 (GCC 7-32)/1749A | Cowardly Rooks/177148059.cpp
@@ -0,0 +1,31 @@
+// Problem: A. Cowardly Rooks
+// Contest: Codeforces - Educational Codeforces Round 138 (Rated for Div. 2)
+ 
+// May the provider keep us content
+ 
+#include<bits/stdc++.h>
+using namespace std ;
+ 
+ 
+int main()
+{
+ int t ;
+ cin >> t ;
+ 
+ while(t--)
+ {
+  int n ;
+  cin >> n ;
+  int m ;
+  cin >> m ;
+  for(int i=0 ;i<m ;i++)
+  {
+   int x , y ;
+   cin >> x >> y ;
+  }
+  if(n>m) cout << "YES" << endl ;
+  else cout << "NO" << endl ;
+ }
+ 
+ 
+}
