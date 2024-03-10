7f7cb9beefef83c4c39a52ff691c9c6d8e866380
Minhajul Abedin Adil <64237828+minhajadil@users.noreply.github.com>
2024-03-10 06:55:26 +0600

    Committing files

---
+++ CodeForces/C++17 (GCC 7-32)/1490C | Sum of Cubes/181159062.cpp
@@ -0,0 +1,56 @@
+// Problem: C. Sum of Cubes
+// Contest: Codeforces - Codeforces Round #702 (Div. 3)
+ 
+// May the provider keep us content
+ 
+#include<bits/stdc++.h>
+using namespace std ;
+ 
+ 
+int main()
+{
+ int t ;
+ cin >> t ;
+ 
+ 
+ 
+ 
+ 
+ while(t--)
+ {
+  long long x ;
+  cin >> x ;
+  
+  unordered_set<long long> c ;
+  
+  for(long long i = 1 ;i*i*i<=1000000000000 ;i++)
+ {
+  c.insert(i*i*i) ;
+ }
+ 
+ 
+ // for(auto it :c)
+ // {
+  // cout << it << " " ;
+ // }
+ // cout << endl ;
+  
+  int y = 0 ;
+  
+  for(long long i = 1 ; i*i*i<x ;i++)
+  {
+   if(c.count(x-(i*i*i))) y=1 ; 
+  }
+  if(y==1) cout << "YES" << endl ;
+  else cout << "NO" << endl ;
+  
+  
+  
+  
+  
+  
+  
+ }
+ 
+ 
+}
