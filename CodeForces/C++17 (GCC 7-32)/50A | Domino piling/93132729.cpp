b2748a951b5cc22d8e6167b76218db8020d19451
Minhajul Abedin Adil <64237828+minhajadil@users.noreply.github.com>
2024-03-10 07:05:16 +0600

    Committing files

---
+++ CodeForces/C++17 (GCC 7-32)/50A | Domino piling/93132729.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std ;
+ 
+int main() {
+int a , b ;
+cin >> a>> b ;
+ 
+int m ,d ;
+ 
+m = a*b ;
+if(m%2==1) m-=1 ;
+ 
+d= m/2 ;
+ 
+cout << d << endl ;
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+}
