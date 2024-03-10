35e5da30747ba51fcc7194c7a987c0c28fda536e
Minhajul Abedin Adil <64237828+minhajadil@users.noreply.github.com>
2024-03-10 06:39:26 +0600

    Committing files

---
+++ CodeForces/C++20 (GCC 11-64)/1909B | Make Almost Equal With Mod/238574641.cpp
@@ -0,0 +1,110 @@
+#include<bits/stdc++.h>
+using namespace std ;
+ 
+//............// define///.................... //
+ 
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+ 
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define endl "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define vc vector
+#define vi vc<int>
+#define vll vc<ll>
+#define pii pair<int,int>
+#define fr(i,a,b) for(ll i=a;i<=b;i++)
+#define coutv(v) for(auto it:v)cout<<it<<' ';cout<<endl;
+#define cinv(v) for(auto &it:v)cin>>it;
+ll llmin(ll a , ll b){if(a<b) return a ; else return b ;}
+ll llmax(ll a , ll b) {if(a>b) return a ; else return b ;}
+ 
+ 
+///...........................//////////////
+ 
+ 
+ 
+ 
+void solve()
+{
+ 
+  int n ; cin >> n ;
+  vector<ll>a(n) ;
+ 
+  for(int i =0 ;i<n ;i++)
+  {
+   cin >>a[i];
+  }
+ 
+ for(ll i =1 ;i<=1000000000000000000 ;i*=2)
+ {
+  map<ll ,ll> cnt ;
+  for(int j =0 ;j<n ;j++)
+  {
+   cnt[a[j]%i]++ ;
+  }
+  if(cnt.size()==2)
+  {
+   cout << i << endl ;
+   return ;
+  }
+ }
+ 
+ 
+ 
+  
+ 
+  
+ 
+  
+}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+int main()
+{
+fastio() ;
+  #ifndef ONLINE_JUDGE
+  freopen("Error.in", "w", stderr);
+#endif
+ 
+ 
+   int t ; cin >> t ;
+   while(t--)
+   {
+ 
+     solve() ;
+    }
+ 
+ 
+ 
+ 
+ 
+}
+ 
+ 
+ 
+ 
