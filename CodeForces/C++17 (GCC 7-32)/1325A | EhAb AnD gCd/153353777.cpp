1b4787f2c477cc6f78cff61ed66b0aa0bdd534b8
Minhajul Abedin Adil <64237828+minhajadil@users.noreply.github.com>
2024-03-10 07:02:27 +0600

    Committing files

---
+++ CodeForces/C++17 (GCC 7-32)/1325A | EhAb AnD gCd/153353777.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std ;
+ 
+int main()
+{
+   int t ;
+   cin >> t ;
+   int n ;
+ 
+   while(t--)
+   {
+    cin >> n ;
+ 
+    cout << n-1 << " " << 1 << endl ;
+ 
+ 
+   }
+ 
+ 
+ 
+}
