70a3f95f4ed973e1b351909d85d2abfc2be4a852
Minhajul Abedin Adil <64237828+minhajadil@users.noreply.github.com>
2024-03-10 06:55:44 +0600

    Committing files

---
+++ CodeForces/C++17 (GCC 7-32)/1748A | The Ultimate Square/180883235.cpp
@@ -0,0 +1,23 @@
+// Problem: A. The Ultimate Square
+// Contest: Codeforces - Codeforces Round #833 (Div. 2)
+ 
+// May the provider keep us content
+ 
+#include<bits/stdc++.h>
+using namespace std ;
+ 
+ 
+int main()
+{
+ int t ;
+ cin >> t ;
+ 
+ while(t--)
+ {
+  int n ;
+  cin >> n ;
+  cout << (n+1)/2 << endl ;
+ }
+ 
+ 
+}
