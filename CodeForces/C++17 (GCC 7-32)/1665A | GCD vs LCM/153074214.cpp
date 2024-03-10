5b7a5c4f278a6130602aed9e7386e72d904a89c6
Minhajul Abedin Adil <64237828+minhajadil@users.noreply.github.com>
2024-03-10 07:02:41 +0600

    Committing files

---
+++ CodeForces/C++17 (GCC 7-32)/1665A | GCD vs LCM/153074214.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std ;
+ 
+ 
+int main()
+{
+ 
+   int t ;
+   cin >> t ;
+ 
+   while(t--)
+   {
+      int n ;
+      cin >>n  ;
+      n=n-3 ;
+ 
+      cout << n << " " << 1 << " " << 1 <<" " << 1 << endl ;
+ 
+ 
+ 
+ 
+   }
+ 
+ 
+ 
+ 
+}
