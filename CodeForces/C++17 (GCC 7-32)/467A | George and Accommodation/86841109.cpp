5a1a184f81a50f8eceb6c7a33309a68dd3c7a34e
Minhajul Abedin Adil <64237828+minhajadil@users.noreply.github.com>
2024-03-10 07:05:39 +0600

    Committing files

---
+++ CodeForces/C++17 (GCC 7-32)/467A | George and Accommodation/86841109.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std ;
+ 
+int main() {
+int t ;
+cin>> t ;
+int counter= 0 ;
+ 
+while(t--) {
+    int a , b ;
+    cin>> a >> b  ;
+    a=a+2 ;
+    if(a<=b) counter++ ;
+ 
+ 
+}
+cout << counter << endl ;
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+return 0 ;
+}
