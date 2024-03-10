3837024648b60db226a7e63f4f33e89914550d74
Minhajul Abedin Adil <64237828+minhajadil@users.noreply.github.com>
2024-03-10 07:00:56 +0600

    Committing files

---
+++ CodeForces/C++17 (GCC 7-32)/1699A | The Third Three Number Problem/162849842.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std ;
+ 
+const int N = 0 ;
+ 
+int main()
+{
+  int t; 
+  cin >> t ;
+ 
+  while(t--)
+  { 
+    long long n ;
+    cin >> n ;
+ 
+ 
+    if(n%2==0 ) {
+        
+        cout << 0 << " " << 0 << " " << n/2 << endl ; 
+    }
+    else 
+    {
+        cout << -1 << endl ;
+    } 
+     
+ 
+ 
+  }
+ 
+ 
+ 
+ 
+ 
+}
