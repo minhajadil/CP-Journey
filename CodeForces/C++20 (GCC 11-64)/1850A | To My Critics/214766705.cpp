072f06241a0482ea91a043edf98cc4d364d558c2
Minhajul Abedin Adil <64237828+minhajadil@users.noreply.github.com>
2024-03-10 06:45:34 +0600

    Committing files

---
+++ CodeForces/C++20 (GCC 11-64)/1850A | To My Critics/214766705.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+using namespace std ;
+ 
+//............// define///.................... //
+ 
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define endl "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define ff first
+#define second ss 
+ 
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+ 
+ 
+ 
+void solve()
+{
+ int a , b ,c ;
+ cin >> a >> b >> c ;
+ if(a+b>=10 || a+c>=10 || b+c>=10) cout <<"YES" << endl;
+ else cout<< "NO" << endl ;
+}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+int main()
+{
+fastio() ;
+  #ifndef ONLINE_JUDGE
+  freopen("Error.in", "w", stderr);
+#endif
+ 
+ 
+   int t ; cin >> t ;
+   while(t--)
+   {
+ 
+     solve() ;
+    }
+ 
+ 
+ 
+ 
+ 
+}
