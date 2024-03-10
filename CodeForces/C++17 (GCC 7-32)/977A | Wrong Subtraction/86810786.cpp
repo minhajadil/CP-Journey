010ca63af57b2d89319ecab2027a3a44e807ddd7
Minhajul Abedin Adil <64237828+minhajadil@users.noreply.github.com>
2024-03-10 07:05:59 +0600

    Committing files

---
+++ CodeForces/C++17 (GCC 7-32)/977A | Wrong Subtraction/86810786.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std ;
+int main()  {
+int n , t ;
+cin>>n >> t ;
+ 
+while(t--) {
+    if(n%10!=0) n=n-1 ;
+    else n=n/10 ;
+ 
+}
+cout << n << endl ;
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+return 0 ;
+}
