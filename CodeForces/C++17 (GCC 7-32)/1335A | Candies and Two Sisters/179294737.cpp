9f7a0ad16c37686553e193ab53d34954c1a84ba2
Minhajul Abedin Adil <64237828+minhajadil@users.noreply.github.com>
2024-03-10 06:56:16 +0600

    Committing files

---
+++ CodeForces/C++17 (GCC 7-32)/1335A | Candies and Two Sisters/179294737.cpp
@@ -0,0 +1,25 @@
+// Problem: A. Candies and Two Sisters
+// Contest: Codeforces - Codeforces Round #634 (Div. 3)
+ 
+// May the provider keep us content
+ 
+#include<bits/stdc++.h>
+using namespace std ;
+ 
+ 
+int main()
+{
+ int t ;
+ cin >> t ;
+ 
+ while(t--)
+ {
+  int n ;
+  cin >> n ;
+  
+  cout << (n-1)/2 << endl ;
+ 
+ 
+}
+ 
+}
