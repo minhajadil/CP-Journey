1b501f821a0c4e8cbeed03805c3cbb6fefa8e884
Minhajul Abedin Adil <64237828+minhajadil@users.noreply.github.com>
2024-03-10 06:47:49 +0600

    Committing files

---
+++ CodeForces/C++20 (GCC 11-64)/1793B | Fedya and Array/204747902.cpp
@@ -0,0 +1,124 @@
+#include<bits/stdc++.h>
+using namespace std ;
+ 
+//............// define///.................... //
+ 
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define endl "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define ff first
+#define second ss 
+ 
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+ 
+///...........................//////////////
+////.......... debugger .........................//
+ 
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+ 
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+//////............................./////////////////
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+// idea is to place the local minimum and maximum in the opposite of the circle and make it 
+// them local min and max by placing elements beside them. now make the other element not local
+// min or max by placing values less than 1 or greater than 1 accordingly.
+ 
+ 
+// idea from Nts
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+int main()
+{
+fastio() ;
+  #ifndef ONLINE_JUDGE
+  freopen("Error.in", "w", stderr);
+#endif
+ 
+ 
+   int t ; cin >> t ;
+   while(t--)
+   {
+ 
+    int mx , mn ;
+    cin  >> mx >> mn ;
+ 
+    cout << 2*(mx-mn-1)+2 << endl ;
+ 
+    for(int i =mx ;i>=mn ;i--)
+    {
+        cout << i << " " ;
+    }
+ 
+    for(int i = mn+1 ;i<mx ;i++)
+    {
+        cout << i << " " ;
+    }
+    cout << endl ;
+    
+ 
+ 
+ 
+    }
+ 
+ 
+ 
+ 
+ 
+}
+ 
+ 
+ 
+ 
+ 
+ 
