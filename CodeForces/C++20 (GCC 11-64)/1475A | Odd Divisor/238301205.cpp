2ed44c1ff5f6275a00b8c6cbde37a909ee6fbdd4
Minhajul Abedin Adil <64237828+minhajadil@users.noreply.github.com>
2024-03-10 06:39:41 +0600

    Committing files

---
+++ CodeForces/C++20 (GCC 11-64)/1475A | Odd Divisor/238301205.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std ;
+ 
+ 
+ 
+ 
+void solve()
+{
+ long long n ; cin >> n ;
+ 
+ while(!(n%2))
+ {
+  n/=2 ;
+ }
+ 
+ if(n>1) cout << "YES" << endl ;
+ else cout << "NO" << endl ;
+ 
+  
+}
+ 
+ 
+ 
+int main()
+{
+ 
+ 
+ 
+   int t ; cin >> t ;
+   while(t--)
+   {
+ 
+     solve() ;
+    }
+ 
+ 
+ 
+ 
+ 
+}
+ 
+ 
+ 
+ 
+ 
+ 
