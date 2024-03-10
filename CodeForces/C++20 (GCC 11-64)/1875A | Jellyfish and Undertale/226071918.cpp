f7a23cda9e64af4e3e668452a961d6780adcb538
Minhajul Abedin Adil <64237828+minhajadil@users.noreply.github.com>
2024-03-10 06:44:04 +0600

    Committing files

---
+++ CodeForces/C++20 (GCC 11-64)/1875A | Jellyfish and Undertale/226071918.cpp
@@ -0,0 +1,106 @@
+#include<bits/stdc++.h>
+using namespace std ;
+ 
+//............// define///.................... //
+ 
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+ 
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define endl "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define vc vector
+#define vi vc<int>
+#define vll vc<ll>
+#define pii pair<int,int>
+#define fr(i,a,b) for(ll i=a;i<=b;i++)
+#define coutv(v) for(auto it:v)cout<<it<<' ';cout<<endl;
+#define cinv(v) for(auto &it:v)cin>>it;
+ll llmin(ll a , ll b){if(a<b) return a ; else return b ;}
+ll llmax(ll a , ll b) {if(a>b) return a ; else return b ;}
+ 
+ 
+///...........................//////////////
+ 
+ 
+ 
+ 
+void solve()
+{
+  int  a , b , n ; cin >> a>>b >> n ;
+ 
+  ll ti[n] ;
+  for(int i =0 ;i<n ;i++)
+  {
+    cin >> ti[i] ;
+  }
+  sort(ti,ti+n) ;
+  //reverse(ti,ti+n) ;
+  ll ans =0 ;
+  ll cur_time =b ;
+ 
+  for(int i =0 ;i<n ;i++)
+  {
+    
+    ans += cur_time-1 ;
+    cur_time = cur_time-(cur_time-1) ;
+    cur_time=llmin(cur_time+ti[i],a) ;
+    
+ 
+  }
+ 
+  cout << ans+(cur_time) << endl ;
+  
+}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+int main()
+{
+fastio() ;
+  #ifndef ONLINE_JUDGE
+  freopen("Error.in", "w", stderr);
+#endif
+ 
+ 
+   int t ; cin >> t ;
+   while(t--)
+   {
+ 
+     solve() ;
+    }
+ 
+ 
+ 
+ 
+ 
+}
+ 
+ 
+ 
+ 
+ 
+ 
