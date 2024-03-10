e6441c89363b8b0befae6cf489d11d8ef2c36846
Minhajul Abedin Adil <64237828+minhajadil@users.noreply.github.com>
2024-03-10 06:51:58 +0600

    Committing files

---
+++ CodeForces/C++17 (GCC 7-32)/507B | Amr and Pins/198341870.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std ;
+ 
+ 
+int main()
+{
+  int r , x, y , p ,q ;
+  cin >> r >> x >> y >> p >> q ;
+ 
+ 
+ 
+ 
+  cout << ceil(hypot(x-p,y-q)/(2*r)) << endl ;
+ 
+ 
+}
