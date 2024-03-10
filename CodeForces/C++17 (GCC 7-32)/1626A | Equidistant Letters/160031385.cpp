7a01ffd11af0d438ba264683b1ff865aedbe6f60
Minhajul Abedin Adil <64237828+minhajadil@users.noreply.github.com>
2024-03-10 07:01:37 +0600

    Committing files

---
+++ CodeForces/C++17 (GCC 7-32)/1626A | Equidistant Letters/160031385.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std ;
+ 
+ 
+int main()
+{
+    int t ;
+    cin >> t ;
+ 
+    while(t--)
+    {
+    string a ;
+    cin >> a ;
+    sort(a.begin(),a.end()) ;
+ 
+    cout << a << endl;
+}
+ 
+ 
+ 
+}
