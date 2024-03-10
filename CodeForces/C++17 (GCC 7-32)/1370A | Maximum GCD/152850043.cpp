6e7adce7f1d98e4e35b165e1f0afc5e6f6583bec
Minhajul Abedin Adil <64237828+minhajadil@users.noreply.github.com>
2024-03-10 07:02:46 +0600

    Committing files

---
+++ CodeForces/C++17 (GCC 7-32)/1370A | Maximum GCD/152850043.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std ;
+ 
+ 
+int main()
+{
+ 
+    int t ;
+    cin >> t ;
+ 
+    while(t--)
+    {
+ 
+    int a ;
+    cin >> a ;
+ 
+    if(a%2!=0) a=a-1 ;
+ 
+    cout << a/2 << endl ;
+}
+   
+ 
+ 
+ 
+ 
+}
