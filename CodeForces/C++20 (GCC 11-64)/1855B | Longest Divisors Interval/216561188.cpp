fe6f8cf1bf81a6ca3e47864630a0e02a435b59bd
Minhajul Abedin Adil <64237828+minhajadil@users.noreply.github.com>
2024-03-10 06:44:53 +0600

    Committing files

---
+++ CodeForces/C++20 (GCC 11-64)/1855B | Longest Divisors Interval/216561188.cpp
@@ -0,0 +1,85 @@
+#include<bits/stdc++.h>
+using namespace std ;
+ 
+//............// define///.................... //
+ 
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define endl "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+ 
+///...........................//////////////
+ 
+ 
+ 
+ 
+void solve()
+{
+  ll n ;
+  cin >> n ;
+ 
+  int cnt =0 ;
+  int i =1 ;
+ 
+  while(n%i==0)
+  {
+    i++ ;
+    cnt++ ;
+  }
+ 
+  cout << cnt << endl ;
+  
+}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+int main()
+{
+fastio() ;
+  #ifndef ONLINE_JUDGE
+  freopen("Error.in", "w", stderr);
+#endif
+ 
+ 
+   int t ; cin >> t ;
+   while(t--)
+   {
+ 
+     solve() ;
+    }
+ 
+ 
+ 
+ 
+ 
+}
+ 
+ 
+ 
+ 
+ 
+ 
