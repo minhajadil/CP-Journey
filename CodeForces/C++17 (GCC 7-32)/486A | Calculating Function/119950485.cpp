a82ed2bc85db4eed975c20f254c733b1bf0500c7
Minhajul Abedin Adil <64237828+minhajadil@users.noreply.github.com>
2024-03-10 07:04:41 +0600

    Committing files

---
+++ CodeForces/C++17 (GCC 7-32)/486A | Calculating Function/119950485.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std ;
+ 
+ 
+int main()
+{
+    
+    long long n ,r,m,t;
+    cin >> n ;
+    if(n%2!=0){ t=n-1 ;
+    r=t/2 ;
+    m=r-n ; }
+    else m = n/2 ;
+    
+    cout << m << endl ;
+    
+    
+    
+    
+}
