6b23eb9230e3776342922fc3a473efe6e08ad248
Minhajul Abedin Adil <64237828+minhajadil@users.noreply.github.com>
2024-03-10 06:44:32 +0600

    Committing files

---
+++ CodeForces/C++20 (GCC 11-64)/1859C | Another Permutation Problem/218644002.cpp
@@ -0,0 +1,114 @@
+#include<bits/stdc++.h>
+using namespace std ;
+ 
+//............// define///.................... //
+ 
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+ 
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define endl "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define vc vector
+#define vi vc<int>
+#define vll vc<ll>
+#define pii pair<int,int>
+#define fr(i,a,b) for(ll i=a;i<=b;i++)
+#define coutv(v) for(auto it:v)cout<<it<<' ';cout<<endl;
+#define cinv(v) for(auto &it:v)cin>>it;
+ll llmin(ll a , ll b){if(a<b) return a ; else return b ;}
+ll llmax(ll a , ll b) {if(a>b) return a ; else return b ;}
+ 
+ 
+///...........................//////////////
+ 
+ 
+ 
+ 
+void solve()
+{
+  int n ; cin >> n ;
+  ll ans = -1 ;
+ 
+  if(n==2)
+  {
+    cout << 2 << endl ;
+    return ;
+  }
+ 
+  fr(i,1,n)
+  {
+    ll sum =0 ;
+    ll mx = -1 ;
+ 
+    fr(j,1,i)
+    {
+        sum+=(j*j) ;
+        mx=llmax(mx,j*j) ;
+    }
+    for(int j =i+1,k=n ;j<=n ;j++,k--)
+    {
+        sum+=(j*k) ;
+        mx=llmax(j*k,mx) ;
+    }
+//    cout << sum << endl ;
+ 
+    ans=llmax(ans,sum-mx) ;
+ 
+  }
+ 
+  cout << ans << endl ;
+  
+}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+int main()
+{
+fastio() ;
+  #ifndef ONLINE_JUDGE
+  freopen("Error.in", "w", stderr);
+#endif
+ 
+ 
+   int t ; cin >> t ;
+   while(t--)
+   {
+ 
+     solve() ;
+    }
+ 
+ 
+ 
+ 
+ 
+}
+ 
+ 
+ 
+ 
+ 
+ 
